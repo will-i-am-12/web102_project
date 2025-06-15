2032b0504bb2155fc6808274d6d86472546bb958
William Lu <luwilliam2006@gmail.com>
2025-06-14 23:22:54 -0400

    Add files via upload

---
+++ w1-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Container from './components/Container'
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <title>CS Career Path</title>
+      <h1 className='title'>CS Career Path</h1>
+      <Container />
+    </div>
+  )
+}
+
+export default App
